bb21a6f52e9c0b6cab79bbcc54cd88baa2dc51c4
RoamingCloud <chenshuyang87@gmail.com>
2024-04-09 15:16:59 +0900

    add llm-sdk project

---
+++ llm-sdk/src/test/java/com/roamingcloud/llmsdk/IOpenAiApiTests.java
@@ -0,0 +1,13 @@
+package com.roamingcloud.llmsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOpenAiApiTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
