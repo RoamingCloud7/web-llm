5d05f20c51b4684aa9d56b1f897528cf72afd5a2
RoamingCloud <chenshuyang87@gmail.com>
2024-04-13 16:08:21 +0900

    update front-end project

---
+++ llm-web/src/apis/index.tsx
@@ -0,0 +1,63 @@
+import { GptVersion } from "@/app/constants";
+import { useAccessStore } from "@/app/store/access";
+import { MessageRole } from "@/types/chat";
+
+const host = "http://localhost:8091";
+
+
+/**
+ * Header 信息
+ */
+function getHeaders() {
+    const accessState = useAccessStore.getState()
+
+    const headers =  {
+        Authorization:  accessState.token,
+        'Content-Type': 'application/json;charset=utf-8'
+    }
+
+    return headers
+}
+
+/**
+ * Role 角色获取接口
+ */
+export const getRoleList = () => {
+    // 从 apiPost mock 接口获取
+    // return fetch(`${host}/role/list`).then((res) =>
+    //     res.json()
+    // );
+
+    // 从本地 json 文件获取
+    return fetch(`/prompts.json`).then((res) => res.json());
+};
+
+/**
+ * 流式应答接口
+ * @param data
+ */
+export const completions = (data: {
+    messages: {content: string; role: MessageRole}[],
+    model: GptVersion
+}) => {
+    return fetch(`${host}/api/v1/chatgpt/chat/completions`, {
+        method: 'post',
+        headers: getHeaders(),
+        body: JSON.stringify(data)
+    });
+};
+
+/**
+ * 登录鉴权接口
+ * @param token
+ */
+export const login = (token: string) => {
+    const accessState = useAccessStore.getState()
+    return fetch(`${host}/api/v1/auth/login`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `code=${accessState.accessCode}`
+    });
+};
--- llm-web/src/app/layout.tsx
+++ llm-web/src/app/layout.tsx
@@ -4,18 +4,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Web - LLM',
-  description: 'LLM网页端应用',
+  title: 'ChatGPT - 小傅哥',
+  description: '您的 ChatGPT 贴心助手！',
 }
 
 export default function RootLayout({
-                                     children,
-                                   }: {
+  children,
+}: {
   children: React.ReactNode
 }) {
   return (
-      <html lang="en">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
-      </html>
+    </html>
   )
 }
